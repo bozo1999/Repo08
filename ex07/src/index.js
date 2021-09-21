bfbc5f98bd823d5e738e282007fd1f699e8f5f5e
bozo1999 <bozo.paragon@gmail.com>
2021-09-21 02:19:12 +0200

    Dodano nesto

---
+++ ex07/src/index.js
@@ -0,0 +1,20 @@
+// Only change code below this line
+var sum = 0;
+function addThree() {
+    sum = sum + 3;
+    console.log(sum);
+}
+addThree();
+
+function addFive() {
+    sum = sum + 5;
+    console.log(sum);
+}
+addFive();
+
+
+// Only change code above this line
+module.exports = {
+    addThree,
+    addFive
+};
---
+++ ex09/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+
+    // Only change code above this line
+    if(a < b) {
+        // Only change code below this line
+
+        // Only change code above this line
+    } else {
+        // Only change code below this line
+
+        // Only change code above this line
+    }
+    // Only change code below this line
+    
+    // Only change code below this line
